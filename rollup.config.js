8116f94479cf5760c229fe372b17ccbedf18b27b
Kenneth Seet <kenneth.seet@autodesk.com>
2025-04-15 15:06:48 +0800

    Fix tests

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,27 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 
-export default {
+export default [
+  // Configuration for index.ts
+  {
     plugins: [nodeResolve(), typescript()],
     input: "src/index.ts",
-    // input: "src/Main.ts",
     output: {
-        plugins: [terser()],
-        dir: "dist",
-        format: "iife",
-        sourcemap: true,
+      plugins: [terser()],
+      dir: "dist",
+      format: "iife",
+      sourcemap: true,
     }
-}
+  },
+  // Configuration for Main.ts
+  {
+    plugins: [nodeResolve(), typescript()],
+    input: "src/Main.ts",
+    output: {
+      plugins: [terser()],
+      dir: "dist", 
+      format: "iife",
+      sourcemap: true,
+    }
+  }
+];
